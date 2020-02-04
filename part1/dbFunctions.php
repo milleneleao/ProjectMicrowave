db73c8e2870e79743d8fa39140ab0dd6cab30981
milleneleao <milleneleao@gmail.com>
2020-02-04 12:18:33 -0500

    create dbfunctions.php

---
+++ part1/dbFunctions.php
@@ -0,0 +1,27 @@
+
+<?php
+
+function validatePath($data){
+	$err_msgs = array();
+    for ($i = 0; $i < count($data); $i++) {
+        echo $data[$i]. "<br />";
+    }
+	return $err_msgs;
+}
+
+function validatePoint($data,$point){
+    $err_msgs = array();
+    for ($i = 0; $i < count($data); $i++) {
+        echo $data[$i]. "<br />";
+    }
+	return $err_msgs;
+}
+
+function validateMidPoints($data){
+    $err_msgs = array();
+    for ($i = 0; $i < count($data); $i++) {
+        echo $data[$i]. "<br />";
+    }
+	return $err_msgs;
+}
+?>
